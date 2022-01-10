719c6cdf92fcd51914ee95ccbd8d4efdccf49c5c
Akwasi Adenkyekye Badu <akwasiade50@gmail.com>
2022-01-10 08:03:15 +0000

    Imported Logos, Icons and Appstring
     Imported logos, icons and appstring
    for  the following screens
    - Home page
    - Exercise
    -Nutrition guide
    -Sleep and relaxation
    -Communication
    -For you/learn

--- lib/src/core/constant/app_strings.dart
+++ lib/src/core/constant/app_strings.dart
@@ -45,10 +45,59 @@ class AppStrings {
   static const verifyMail = 'Verify your email';
 
   ///Home
+  static const welcome = "Welcome";
+  static const  canWeHelp = "What can we help with today?";
+  static const  categories = "Categories";
+  static const sleepRelaxation = "Sleep & Relaxation";
+  static const nutritionGuide = "Nutrition guide";
+  static const exercise  = "Exercise";
+  static const explore = "Explore";
 
   ///For You
+  static const forYou  = "For you";
+  static const forYouText1 = "Taking this first step may feel daunting and overwhelming but we’ll take your hand every step of the way";
+  static const audio = "Audios/Podcasts";
+  static const articles  = "Articles";
+  static const takeQuiz  = "Take a quiz";
+  static const takeCourse =  "Take a course";
 
   ///MindBot
 
   ///Profile
+///Exercise
+  static const exerciseText1 = "Physical exercises promote rest, improve sleep, regulate mood and help manage stress.";
+  static const yoga = "Yoga";
+  static const aerobics = "Aerobics";
+  static const resistanceTraining = "Resistance training";
+  static const pilates = "Pilates";
+  static const boxing = "Boxing";
+  static const exerciseText2 = "Find out more about how physical exercises improves mental health here.";
+
+
+  ///Nutrition
+  static const nutrition = "Nutrition";
+  static const nutritionText1 = "Nourish your mind and body with healthy diet guidelines.";
+  static const vegetarianDiets = "Vegetarian Diets";
+  static const nutritionSubText1 = "Enjoy carefully selected songs and instrumentals to";
+  static const supplements = "Supplements";
+  static const nutritionSubText2 = "Enjoy carefully selected songs and instrumentals to prompts relaxation and restful sleep";
+  static const specialDiets = "Learn about diets from around theworld proven toimprove mental well being.";
+  static const nutritionText2 = "Find out more about how diet can improve mental health here.";
+
+
+  //Sleep Relaxation
+  static const sleep = "Sleep";
+  static const sleepText1 = "Mental health disorders affect sleep in different ways; lack of proper sleep and rest aggravates mental health problems.";
+  static const sleepSubText1 = "10 ways to improve sleep.";
+  static const sleepSubText2 = "The importance of rest.";
+
+
+  // Community
+  static const community = "Community";
+  static const communityText1 = "Staying grounded in communities not only provides a safe space for recovery but also improves therapy outcomes.";
+  static const  joinMember = "Join as a member";
+  static const communitySubText1 = "You don’t have to go on this journey alone.";
+  static const communityText2 = "Join as a therapist";
+  static const  alreadyMember = "Already a member? Log in";
+
 }
