6fdaee6f289042c2493e5affad1db67a28693ef3
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2022-01-22 13:34:05 +0100

    Revert "Bottom navigation bar implementation (#11)" (#12)
    This reverts commit f34311f94a9087b28657e093f9351222e0c528d8.

--- lib/src/features/home/view/home_view.dart
+++ lib/src/features/home/view/home_view.dart
@@ -12,65 +12,38 @@ class HomeView extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     return Statusbar(
         child: Scaffold(
-      body: Column(
-        children: [
-          Flexible(
-            child: CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  automaticallyImplyLeading: false,
-                  backgroundColor: Theme.of(context).colorScheme.primary,
-                  expandedHeight: height * 0.15,
-                  title: Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: ListTile(
-                      title: Text(
-                        '${AppStrings.welcome} ${StartupNotifier().currentUser?.displayName}',
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                      subtitle: Text(
-                        AppStrings.canWeHelp,
-                        style: Theme.of(context).textTheme.headline2!.copyWith(
-                              fontSize: 12,
-                            ),
-                      ),
-                    ),
-                  ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            expandedHeight: height * 0.15,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: ListTile(
+                title: Text(
+                  '${AppStrings.welcome} ${StartupNotifier().currentUser?.displayName}',
+                  style: Theme.of(context).textTheme.headline2,
                 ),
-              ],
-            ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppStrings.categories,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4!
-                        .copyWith(fontSize: 20),
-                  ),
-                  Flexible(
-                    child: ListView.builder(
-                      itemCount: categories.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          color: Theme.of(context).colorScheme.primary,
-                          child: Row(
-                            children: [Text(categories[index])],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
+                subtitle: Text(
+                  AppStrings.canWeHelp,
+                  style: Theme.of(context).textTheme.headline2!.copyWith(
+                        fontSize: 12,
+                      ),
+                ),
               ),
             ),
           ),
- 
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Card(
+                color: Theme.of(context).colorScheme.primary,
+                child: Row(
+                  children: [Text(categories[index])],
+                ),
+              );
+            }, childCount: categories.length),
+          ),
         ],
       ),
     ));
